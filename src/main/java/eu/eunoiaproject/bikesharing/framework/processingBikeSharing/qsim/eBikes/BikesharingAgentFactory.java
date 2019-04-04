107fbbf484f72d515fecf762db0033709f44531b
kainagel <nagel@vsp.tu-berlin.de>
2019-04-04 12:04:06 +0200

    remove commented out code

--- src/main/java/eu/eunoiaproject/bikesharing/framework/processingBikeSharing/qsim/eBikes/BikesharingAgentFactory.java
+++ src/main/java/eu/eunoiaproject/bikesharing/framework/processingBikeSharing/qsim/eBikes/BikesharingAgentFactory.java
@@ -14,37 +14,6 @@ public class BikesharingAgentFactory implements AgentFactory{
 
 	@Override
 	public MobsimDriverAgent createMobsimAgentFromPerson(final Person p) {
-		
-		String type = p.getSelectedPlan().getType();
-		
-		//VehicleImpl vehicle =  new VehicleImpl(Id.createVehicleId(p.getId().toString()), null);
-		//MobsimVehicle veh = (MobsimVehicle) vehicle;
-		MobsimDriverAgent agent;
-		
-		if (type == null)
-		{
-			throw new RuntimeException("There must be a plan type set in the plans file for the use of bike sharing, please do so!");
-//			System.exit(0);
-			// if System.exit(...) is encountered in regression test, one gets weird behavior.  kai, apr'19
-		}
-//		QSim qsim = new QSim(scenario, eventsManager);
-		
-		//Hebenstreit TODO:
-//		if ((type.equals("ffBikeSharing"))||
-//			type.equals("eBikeSharing") || type.equals(TransportMode.pt))
-		{ 
-			agent = new BikesharingPersonDriverAgentImpl(p.getSelectedPlan(), null, bikeSharingContext );
-		}
-		//if (type.equals("pt"))
-		//{
-		//	agent = TransitAgent.createTransitAgent(p, this.simulation); 
-		//}
-//		else
-//		{
-//			agent = new PersonDriverAgentImpl(p.getSelectedPlan(), this.simulation);
-//		}
-		
-//		qsim.insertAgentIntoMobsim(agent);
-		return agent;
+		return new BikesharingPersonDriverAgentImpl( p.getSelectedPlan(), null, bikeSharingContext );
 	}
 }
